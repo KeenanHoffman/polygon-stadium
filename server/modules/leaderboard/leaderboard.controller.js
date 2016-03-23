44c8ea3e1aac83377e7da9ef93c39a5872a7843f
KeenanHoffman <keenan.m.hoffman@gmail.com>
2016-03-23 16:57:52 -0600

    adding else to avoid double sending res

--- server/modules/leaderboard/leaderboard.controller.js
+++ server/modules/leaderboard/leaderboard.controller.js
@@ -2,7 +2,7 @@
 
 function getLeaderboard(req, res, next) {
   req.models.saved_game.find().sort('score DESC').limit(10).populate('player').exec(function(err, scores) {
-    if (err) next(err);
+    if (err) next(err); else 
     if (scores) {
       scores.forEach(function(score) {
         delete score.player[0].password;
