91a0aea4f7e85bd8c5c26a55edbe6dd8ea7387b2
KeenanHoffman <keenan.m.hoffman@gmail.com>
2016-03-23 12:25:18 -0600

    added checking for lack of scrores in leaderboard route

--- server/modules/leaderboard/leaderboard.controller.js
+++ server/modules/leaderboard/leaderboard.controller.js
@@ -3,12 +3,18 @@
 function getLeaderboard(req, res, next) {
   req.models.saved_game.find().sort('score DESC').limit(10).populate('player').exec(function(err, scores) {
     if (err) next(err);
-    scores.forEach(function(score) {
-      delete score.player[0].password;
-      delete score.player[0].email;
-      delete score.user_id;
-    });
-    res.send(scores);
+    if (scores) {
+      scores.forEach(function(score) {
+        delete score.player[0].password;
+        delete score.player[0].email;
+        delete score.user_id;
+      });
+      res.json(scores);
+    } else {
+      res.json({
+        status: 'no scores where found'
+      });
+    }
   });
 }
 
